6814f1fa69e4108d87ce32b416b01dbffe35aab4
Stefano Kowalke <stefan.kowalke@swisscom.com>
2020-06-26 12:16:01 +0200

    Backport to TYPO3 8.7

--- ext_localconf.php
+++ ext_localconf.php
@@ -14,22 +14,6 @@ call_user_func(function () {
         '));
     }
 
-    $typo3Version = new \TYPO3\CMS\Core\Information\Typo3Version();
-    if ($typo3Version->getMajorVersion() === 10) {
-        // load additional YAML configuration to load translation files differently for T3 v10
-        if (TYPO3_MODE === 'BE') {
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(trim('
-            module.tx_form {
-                settings {
-                    yamlConfigurations {
-                        1587917063 = EXT:form_element_linked_checkbox/Configuration/Yaml/FormSetupV10.yaml
-                    }
-                }
-            }
-        '));
-        }
-    }
-
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeRendering'][1571076908]
         = \TRITUM\FormElementLinkedCheckbox\Hooks\FormElementLinkResolverHook::class;
 });
