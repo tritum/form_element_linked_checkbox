36da0799f8500225eb38941cb6a0b891fdd0490d
Stefano Kowalke <stefan.kowalke@swisscom.com>
2020-07-02 17:30:50 +0200

    [BUGFIX] Add the correct CSS classes to the link (BJBWU-869)

--- Classes/Hooks/FormElementLinkResolverHook.php
+++ Classes/Hooks/FormElementLinkResolverHook.php
@@ -150,6 +150,7 @@ class FormElementLinkResolverHook
         $configuration = [
             'typolink.' => [
                 'parameter' => trim($parameter),
+                'ATagParams' => 'class="legal-link"',
             ],
         ];
         if ($additionalAttributes) {
