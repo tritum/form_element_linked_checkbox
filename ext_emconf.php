74137c00e79f2088979e857fd6c7237a6eafa488
Björn Jacob <bjoern.jacob@tritum.de>
2021-12-15 17:48:16 +0100

    [TASK] Raise TYPO3 requirements and version bump
    The TYPO3 versions 9.5.30 and 10.4.20 included several security
    bugfixes within the TYPO3 core. This patch raises the requirement
    to the above mentioned TYPO3 verions.
    
    In addition, the version has been bumped to 2.2.0.

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF['form_element_linked_checkbox'] = [
     'clearCacheOnLoad' => 0,
     'author' => 'Bjoern Jacob TRITUM GmbH',
     'author_email' => 'bjoern.jacob@tritum.de',
-    'version' => '2.1.2',
+    'version' => '2.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.25-11.99.99',
+            'typo3' => '9.5.30-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
