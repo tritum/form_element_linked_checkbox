e8044914265b2c69bbfb9df1becef737611b9a20
Tobias Jungmann <186044798+dreistromland-tj@users.noreply.github.com>
2024-11-21 11:35:24 +0100

    [BUGFIX] #73 Fix backend JavaScript
    The loading of JavaScript in backend context has changed with TYPO3 v12
    and the old method has been removed in v13.
    Also, the JavaScript of EXT:form has changed and the ViewModel.js has
    been adapted.
    See https://docs.typo3.org/c/typo3/cms-form/main/en-us/I/Concepts/FormEditor/Index.html#concepts-formeditor-basicjavascriptconcepts-registercustomjavascriptmodules

---
+++ Configuration/JavaScriptModules.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'dependencies' => ['form'],
+    'imports' => [
+        '@tritum/form-element-linked-checkbox/' => 'EXT:form_element_linked_checkbox/Resources/Public/JavaScript/'
+    ],
+];
