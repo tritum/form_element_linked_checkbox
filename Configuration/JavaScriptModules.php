972e2f8ef922f7630efb7f500a57816298389db2
Tobias Jungmann <186044798+dreistromland-tj@users.noreply.github.com>
2024-11-21 12:03:57 +0100

    [CLEANUP] #73 Fix code style

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -3,6 +3,6 @@
 return [
     'dependencies' => ['form'],
     'imports' => [
-        '@tritum/form-element-linked-checkbox/' => 'EXT:form_element_linked_checkbox/Resources/Public/JavaScript/'
+        '@tritum/form-element-linked-checkbox/' => 'EXT:form_element_linked_checkbox/Resources/Public/JavaScript/',
     ],
 ];
